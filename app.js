ae3f18872a494ec83d8fe5afa6ca4fd37b268249
Shrikant Yadav <shrikant9907@gmail.com>
2024-11-18 10:03:48 +0530

    fix cors error

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('./src/config/db');
 const todoRoutes = require('./src/routes/todoRoutes');
 const errorHandlerMiddleware = require('./src/middlewares/errorHandlerMiddleware');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Define the CORS options
+const corsOptions = {
+  credentials: true,
+  origin: ['http://localhost:3000', 'http://localhost:80']
+};
+
+app.use(cors(corsOptions));
 
 // Connect to MongoDB
 connectDB();
